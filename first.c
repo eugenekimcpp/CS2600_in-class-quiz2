0fac2213dd0c76536d0d0ebe6ffb3648e6981e90
Eugene Kim <eugenekim@cpp.edu>
2022-10-05 19:54:08 -0700

    First commit with comments

---
+++ first.c
@@ -0,0 +1,37 @@
+/* 
+    Program: first.c
+    Programmer: Eugene Kim
+
+    This program practices the game called "Tic-Tac-Toe"
+
+    Two level: 
+    * person vs. person 
+    * person vs. random computer
+
+
+
+    First approach: 
+    1. Create a function that keep displays the template, using printf()
+    2. use random() NOT suedo random () (range 1-9)
+    3. use switch cases for the user input
+
+*/
+
+int main() {
+
+    // create variables & template 
+
+
+    // prompt the user for game they wish to play
+
+
+    // read information from console
+
+
+    
+
+
+
+    return 0;
+    
+}
